5cb9ba30ffbaea425525704f76d05b710e976c44
AhuraMagda <lubaszka.magda@gmail.com>
2023-12-17 08:44:41 +0100

    gameboard layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
-import './App.css'
+import GameBoard from './components/GameBoard'
+import './scss/main/styles.css'
 
 function App() {
 
   return (
     <>
       <h1>Code breaking game</h1>
-      <h2>header</h2>
-      <p>- ? ? ? ? </p>
-      <p>- timer</p>
-      <p>- moves</p>
-      <h2>game board</h2>
-      <p>- empty slots</p>
-      <p>- correct and partly correct guesses</p>
-      <h2>nums pick</h2>
+
+      <GameBoard />
     </>
   )
 }
---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,55 @@
+export default function GameBoard() {
+  return (
+    <div className="game-board">
+      <div className="game-board__row">
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+      </div>
+      <div className="game-board__row">
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+      </div>
+      <div className="game-board__row">
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+      </div>
+      <div className="game-board__row">
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+      </div>
+      <div className="game-board__row">
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+      </div>
+      <div className="game-board__row">
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+      </div>
+      <div className="game-board__row">
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+        <div className="game-board__row__circle"></div>
+      </div>
+    </div>
+  );
+}
